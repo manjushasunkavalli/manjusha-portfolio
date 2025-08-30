751c4a6814afa808853a42f331f790a226dec20b
Manjusha Devi Sunkavalli <manjushadevisunkavalli3@gmail.com>
2025-08-30 18:59:39 -0400

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       icon: <Activity className="w-8 h-8 text-blue-600" />,
       category: "Healthcare Analytics",
       company: "Accenture (Client: Cigna)"
-    },
+    }
     {
   title: "IoT Data Engineering in Manufacturing",
   description: "Designed and optimized SQL queries and ETL workflows for integrating IoT sensor and financial data. Delivered Power BI dashboards to provide supply chain and operational insights, improving reporting efficiency and decision-making.",
